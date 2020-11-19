b893e38990eb1de58e449a46a47eb96d496c4b5f
Mans <mnsosk-7@student.ltu.se>
2020-11-19 14:06:11 +0100

    fix3

--- examples/server_example.js
+++ examples/server_example.js
@@ -11,17 +11,17 @@ const kittySchema = new mongoose.Schema({
     name: String
 });
 
+kittySchema.methods.speak = function () {
+  const greeting = this.name
+    ? "Meow name is " + this.name
+    : "I don't have a name";
+  console.log(greeting);
+}
+
 const Kitten = mongoose.model('Kitten', kittySchema);
 const silence = new Kitten({ name: 'Silence' });
 
 console.log(silence.name);
-
-kittySchema.methods.speak = function () {
-    const greeting = this.name
-      ? "Meow name is " + this.name
-      : "I don't have a name";
-    console.log(greeting);
-}
 
 const fluffy = new Kitten({ name: 'fluffy' });
 
