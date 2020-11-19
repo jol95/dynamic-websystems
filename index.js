0f7d1a62c9e425f620c9e46ef908a756f38aef7c
Måns Oskarsson <mnsosk-7@student.ltu.se>
2020-11-19 09:58:17 +0000

    Init

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+let app = express();
+
+var port = process.env.PORT || 8080;
+
+mongoose.connect('mongodb://localhost/mongodb', {useNewUrlParser: true, useUnifiedTopology: true });
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+	 //  we're connected!
+});
+
+// Welcome message
+app.get('/', (req, res) => res.send('Welcome to Express'));
+
+let apiRoutes = require("./routes");
+
+app.use('/api', apiRoutes);
+
+// Launch app to the specified port
+app.listen(port, function() {
+     console.log("Running FirstRest on Port "+ port)
+})
+
