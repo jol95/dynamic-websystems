6d1233fadc95dcf833803d933c55984e8bce0cc5
Mans <mnsosk-7@student.ltu.se>
2020-12-12 15:16:30 +0100

    added prosumer

--- server/backend/server.js
+++ server/backend/server.js
@@ -7,7 +7,7 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 const app = express();
-const port = process.env.PORT || 8082;
+const port = process.env.PORT || 8083;
 
 app.use(cors());
 app.use(express.json());
