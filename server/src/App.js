fee7f75d0c9e92fc39803eb2c22638a08d48ca0d
Mans <mnsosk-7@student.ltu.se>
2020-12-13 22:06:24 +0100

    fixed front end

--- server/src/App.js
+++ server/src/App.js
@@ -1,4 +1,25 @@
-import React, {Component} from 'react'
+import React from "react";
+import Navbar from "react-bootstrap/Navbar";
+import Routes from "./Routes";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App container py-3">
+      <Navbar collapseOnSelect bg="light" expand="md" className="mb-3">
+        <Navbar.Brand className="font-weight-bold text-muted">
+          Scratch
+        </Navbar.Brand>
+        <Navbar.Toggle />
+      </Navbar>
+      <Routes />
+    </div>
+  );
+}
+
+export default App;
+
+/* import React, {Component} from 'react'
 import Table from './Table'
 import Form from './Form'
 
@@ -35,6 +56,4 @@ class App extends Component {
         )
       }
 }
-
-
-export default App
+export default App */
--- server/src/index.js
+++ server/src/index.js
@@ -1,6 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './Api'
+import { BrowserRouter as Router } from 'react-router-dom';
+import App from './App'
 import './index.css'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(
+    <React.StrictMode>
+      <Router>
+        <App />
+      </Router>
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
