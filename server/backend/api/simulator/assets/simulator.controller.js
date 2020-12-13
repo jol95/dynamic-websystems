cdb28985e5053f789a70d8591cba9485cd63766a
Mans <mnsosk-7@student.ltu.se>
2020-12-13 14:59:51 +0100

    fixed prosumerSchema and added api endpoint for simulator

--- server/backend/api/simulator/assets/simulator.controller.js
+++ server/backend/api/simulator/assets/simulator.controller.js
@@ -1,33 +1,11 @@
 const mongoose = require('mongoose');
 
-let User = require('./user.model');
-let Prosumer = require('./prosumer.model');
-
-exports.getUser = function(req, res) {
-    User.find()
-        .then(users => res.json(users))
-        .catch(err => res.status(400).json('Error: ' + err));
+exports.getHouses = function(req, res) {
+    
 }
 
-exports.registerUser = function(req, res) {
+exports.addHouse = function(req, res) {
     const houseid = new mongoose.mongo.ObjectId()
-
-    const username = req.body.username;
-    const password = req.body.password;
-    const firstname = req.body.firstname;
-    const lastname = req.body.lastname;
-    const address = req.body.address;
-
-    const newUser = new User({
-        username,
-        password,
-        firstname,  
-        lastname,
-        houseid,
-        address
-    });
-
-    newUser.save()
 
     const currentwind = 0 
     const currentproduction = 0  
@@ -60,3 +38,8 @@ exports.getHouse = function(req, res) {
         }
     });
 }
+
+exports.updateHouse = function(req, res) {
+    
+}
+
