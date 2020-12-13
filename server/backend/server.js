8437b3488750e8511ebbd9b69f577f404fd97465
Mans <mnsosk-7@student.ltu.se>
2020-12-13 23:13:43 +0100

    testest

--- server/backend/server.js
+++ server/backend/server.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 require('dotenv').config();
 
@@ -9,7 +10,8 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(cors());
-app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: false}));
+app.use(bodyParser.json());
 
 /* ----------MongoDB------------*/
 let dbPath = 'mongodb://localhost/mydb';
