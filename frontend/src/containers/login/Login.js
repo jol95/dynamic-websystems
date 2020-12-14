8442745a6c0e92bd8631a18d887079ea33552ba4
Mans <mnsosk-7@student.ltu.se>
2020-12-15 00:28:12 +0100

    Fixed login

--- frontend/src/containers/login/Login.js
+++ frontend/src/containers/login/Login.js
@@ -21,7 +21,7 @@ function Login(props) {
   
     console.log(body)
   
-    axios.get('/api/user', {body})
+    fetch('/api/user', {body})
     .then(function (response) {
       console.log(response);
     })
