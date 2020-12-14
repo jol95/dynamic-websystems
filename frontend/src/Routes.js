28412d84185b3435e1193bb8b0d10c5a316c6b27
Mans <mnsosk-7@student.ltu.se>
2020-12-14 12:33:07 +0100

    for merge

--- frontend/src/Routes.js
+++ frontend/src/Routes.js
@@ -1,16 +1,12 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 import Home from "./containers/home/Home";
-import Login from "./containers/login/Login";
 
 export default function Routes() {
   return (
     <Switch>
       <Route exact path="/">
         <Home />
-      </Route>
-      <Route exact path="/login">
-        <Login />
       </Route>
     </Switch>
   );
