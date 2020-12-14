6725da849310ab5ca84404c98f7ca5c6fbf0cb3c
Mans <mnsosk-7@student.ltu.se>
2020-12-14 23:44:49 +0100

    body fix

--- frontend/src/containers/login/Login.js
+++ frontend/src/containers/login/Login.js
@@ -4,32 +4,24 @@ import Button from "react-bootstrap/Button";
 import axios from 'axios';
 import "./Login.css";
 
-class Login extends React.Component{
-  constructor(props){
-    super(props);
+export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-    this.state = {
-      email:"",
-      password:"",
-      newEmail:"",
-      newPassword:""
-    }
+  function validateForm() {
+    return email.length > 0 && password.length > 0;
   }
 
-  validateForm = (e) => {
-    return this.state.email.length > 0 && this.state.password.length > 0;
-  }
-
-  handleSubmit = (e) => {
-    e.preventDefault();
+  function handleSubmit(event) {
+    event.preventDefault();
 
     const body = {
       email: this.state.email,
       password: this.state.password
     };
-
+  
     console.log(body)
-
+  
     axios.get('/api/user', {body})
     .then(function (response) {
       console.log(response);
@@ -37,38 +29,33 @@ class Login extends React.Component{
     .catch(function (error) {
       console.log(error);
     });
-
-    
   }
 
-  render(){
-    return (
-      <div className="Login">
-        <Form onSubmit={this.handleSubmit}>
-          <Form.Group size="lg" controlId="email">
-            <Form.Label>Email</Form.Label>
-            <Form.Control
-              autoFocus
-              type="email"
-              onChange={(e, newValue) => this.setState({password: newValue})}
-            />
-          </Form.Group>
-          <Form.Group size="lg" controlId="password">
-            <Form.Label>Password</Form.Label>
-            <Form.Control
-              type="password"
-              onChange={(e, newValue) => this.setState({password: newValue})}
-            />
-          </Form.Group>
-          <Button block size="lg" type="submit" disabled={!this.validateForm}>
-            Login
-          </Button>
-        </Form>
-      </div>
-    );
-  }
+  return (
+    <div className="Login">
+      <Form onSubmit={handleSubmit}>
+        <Form.Group size="lg" controlId="email">
+          <Form.Label>Email</Form.Label>
+          <Form.Control
+            autoFocus
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </Form.Group>
+        <Form.Group size="lg" controlId="password">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </Form.Group>
+        <Button block size="lg" type="submit" disabled={!validateForm()}>
+          Login
+        </Button>
+      </Form>
+    </div>
+  );
 }
-
-export default Login
-
 
