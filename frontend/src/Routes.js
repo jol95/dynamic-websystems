a17a0f098d9d259acea787c438989531d529ad7f
Mans <mnsosk-7@student.ltu.se>
2020-12-14 01:00:22 +0100

    fix project

--- frontend/src/Routes.js
+++ frontend/src/Routes.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
-import Home from "./containers/Home";
+import Home from "./containers/home/Home";
 
 export default function Routes() {
   return (
