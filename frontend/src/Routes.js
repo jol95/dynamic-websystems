950813bea26e015f43c065062796f5179ad8d9ca
Mans <mnsosk-7@student.ltu.se>
2020-12-14 16:14:04 +0100

    added not found fix for file direct

--- frontend/src/Routes.js
+++ frontend/src/Routes.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 import Home from "./containers/home/Home";
-import NotFound from "./containers/NotFound";
+import NotFound from "./containers/default/NotFound";
 
 export default function Routes() {
   return (
