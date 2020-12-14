71913bc3fa77ff26b7ecd11f62ca71e72b9d4857
Mans <mnsosk-7@student.ltu.se>
2020-12-15 00:01:55 +0100

    check body in api when querying from react

--- backend/api/user/assets/user.controller.js
+++ backend/api/user/assets/user.controller.js
@@ -4,7 +4,7 @@ let User = require('./user.model');
 let Prosumer = require('./prosumer.model');
 
 exports.getUser = function(req, res) {
-    User.findOne({ email: req.body.email, password: req.body.password}, 
+    await User.findOne({ email: req.body.email, password: req.body.password}, 
     function (err, user) {
         if (err){
             console.log(err);
