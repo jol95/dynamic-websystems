9670f09a2cd9680925f2b020990c3e4f2aa07a17
Mans <mnsosk-7@student.ltu.se>
2020-12-15 00:25:57 +0100

    Fixed login

--- frontend/src/containers/login/Login.js
+++ frontend/src/containers/login/Login.js
@@ -15,8 +15,8 @@ function Login(props) {
     //props.history.push('/dashboard');
 
     const body = {
-      email: email.value,
-      password: password.value
+      "email": email.value,
+      "password": password.value
     };
   
     console.log(body)
