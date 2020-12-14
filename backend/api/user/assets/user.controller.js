e1969fb38f5fac8824b2fb881beb1aa779b68453
Mans <mnsosk-7@student.ltu.se>
2020-12-14 12:34:49 +0100

    for merge

--- backend/api/user/assets/user.controller.js
+++ backend/api/user/assets/user.controller.js
@@ -4,7 +4,7 @@ let User = require('./user.model');
 let Prosumer = require('./prosumer.model');
 
 exports.getUser = function(req, res) {
-    User.find({ username: req.params.username, password: req.params.password}, 
+    User.find({ username: req.params.username}, 
     function (err, user) {
         if (err){
             console.log(err);
