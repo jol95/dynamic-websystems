d99af3b19b3086bd419d277ab0be9ed48627f9d4
Mans <mnsosk-7@student.ltu.se>
2020-12-15 00:08:18 +0100

    Fixed login

--- frontend/src/containers/login/Login.js
+++ frontend/src/containers/login/Login.js
@@ -4,16 +4,15 @@ import Button from "react-bootstrap/Button";
 import axios from 'axios';
 import "./Login.css";
 
-export default function Login() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-
-  function validateForm() {
-    return email.length > 0 && password.length > 0;
-  }
-
-  async function handleSubmit(event) {
-    event.preventDefault();
+function Login(props) {
+  const email = useFormInput('');
+  const password = useFormInput('');
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+ 
+  // handle button click of login form
+  const handleLogin = () => {
+    props.history.push('/dashboard');
 
     const body = {
       email: email,
@@ -30,32 +29,53 @@ export default function Login() {
       console.log(error);
     });
   }
-
+ 
   return (
-    <div className="Login">
-      <Form onSubmit={handleSubmit}>
-        <Form.Group size="lg" controlId="email">
-          <Form.Label>Email</Form.Label>
-          <Form.Control
-            autoFocus
-            type="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-        </Form.Group>
-        <Form.Group size="lg" controlId="password">
-          <Form.Label>Password</Form.Label>
-          <Form.Control
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </Form.Group>
-        <Button block size="lg" type="submit" disabled={!validateForm()}>
-          Login
-        </Button>
-      </Form>
+    <div>
+      Login<br /><br />
+      <div>
+        Email<br />
+        <input type="text" {...email} autoComplete="new-password" />
+      </div>
+      <div style={{ marginTop: 10 }}>
+        Password<br />
+        <input type="password" {...password} autoComplete="new-password" />
+      </div>
+      {error && <><small style={{ color: 'red' }}>{error}</small><br /></>}<br />
+      <input type="button" value={loading ? 'Loading...' : 'Login'} onClick={handleLogin} disabled={loading} /><br />
     </div>
   );
 }
+ 
+const useFormInput = initialValue => {
+  const [value, setValue] = useState(initialValue);
+ 
+  const handleChange = e => {
+    setValue(e.target.value);
+  }
+  return {
+    value,
+    onChange: handleChange
+  }
+}
+ 
+export default Login;
 
+async function handleSubmit(event) {
+  event.preventDefault();
+
+  const body = {
+    email: email,
+    password: password
+  };
+
+  console.log(body)
+
+  axios.get('/api/user', {body: body})
+  .then(function (response) {
+    console.log(response);
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
+}
