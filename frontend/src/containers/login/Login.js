1f5082fb43feeef933207b0b2462902390be6697
Mans <mnsosk-7@student.ltu.se>
2020-12-15 14:16:44 +0100

    Login.css

--- frontend/src/containers/login/Login.js
+++ frontend/src/containers/login/Login.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import axios from 'axios';
-import "./Register.css";
+import "./Login.css";
 
 function Login(props) {
   const email = useFormInput('');
