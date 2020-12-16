f97221fb9cee4cfe8f37f9aff1b3e9d2f75a7be3
Mans <mnsosk-7@student.ltu.se>
2020-12-16 16:35:23 +0100

    test

--- backend/api/user/user.js
+++ backend/api/user/user.js
@@ -4,8 +4,8 @@ const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const keys = require("../../config/keys.js");
 
-const validateRegisterInput = require("/assets/validation/register");
-const validateLoginInput = require("/assets/validation/login");
+const validateRegisterInput = require("./assets/validation/register");
+const validateLoginInput = require("./assets/validation/login");
 
 var userController = require("./assets/user.controller.js")
 
