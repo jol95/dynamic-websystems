6fe5772dee519fcf76f2df086e5f855b2e6a7b04
Mans <mnsosk-7@student.ltu.se>
2020-12-16 23:35:48 +0100

    updating register

--- simulator/simulator.js
+++ simulator/simulator.js
@@ -1,23 +1,29 @@
 const distribute = require("./old/distribute");
 const axios = require("axios");
+const { response } = require("express");
 
 const backend = "http://localhost:5000/api"
 
 tick = 5000;
 setInterval(() => {
+    let houses
     axios.get(backend + '/simulator')
     .then(function (response) {
-      console.log(response);
       console.log(response.data)
+      let houses = response.data;
     })
     .catch(function (error) {
       console.log(error);
     });
 
-        distribute.distributeAvg();
-        console.log(distribute.wDay);
-        console.log("^^ WIND ^^");
-        console.log(distribute.cons);
-        console.log("^^ CONS^^");
+    houses.map(house) => {
+        console.log(h)
+    };
+
+    distribute.distributeAvg();
+    console.log(distribute.wDay);
+    console.log("^^ WIND ^^");
+    console.log(distribute.cons);
+    console.log("^^ CONS^^");
     }, tick);
 
