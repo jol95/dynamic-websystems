8d3863892a4e28c1c4e1b0219c3c7ab373b5a234
Mans <mnsosk-7@student.ltu.se>
2020-12-17 00:32:11 +0100

    login token auth first try

--- simulator/simulator.js
+++ simulator/simulator.js
@@ -5,23 +5,24 @@ const backend = "http://localhost:5000/api"
 
 tick = 5000;
 setInterval(() => {
-    let houses
     axios.get(backend + '/simulator')
     .then(function (response) {
       let houses = response.data;
     })
     .catch(function (error) {
       console.log(error);
-    });
+  });
 
-    houses.map( function(house) {
-        console.log(house);
-    });
+  console.log
 
-    distribute.distributeAvg();
-    console.log(distribute.wDay);
-    console.log("^^ WIND ^^");
-    console.log(distribute.cons);
-    console.log("^^ CONS^^");
-    }, tick);
+  //houses.map(house => {
+    //house.
+  //})
+
+  distribute.distributeAvg();
+  console.log(distribute.wDay);
+  console.log("^^ WIND ^^");
+  console.log(distribute.cons);
+  console.log("^^ CONS^^");
+}, tick);
 
