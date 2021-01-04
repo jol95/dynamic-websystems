7e42bd561c5f3fdd9995d94a5aa42fc90b6d1da6
Mans <mnsosk-7@student.ltu.se>
2021-01-04 15:26:57 +0100

    changed routes and fix

--- backend/api/user/assets/user.controller.js
+++ backend/api/user/assets/user.controller.js
@@ -3,7 +3,7 @@ const express = require("express");
 const router = express.Router();
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
-const keys = require("../../../config/keys");
+const keys = require("../config/keys");
 
 let User = require('./user.model');
 let Household = require('./household.model');
