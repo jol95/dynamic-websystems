02591d33037bbc2d30d32692628842d0ce7ca504
Mans <mnsosk-7@student.ltu.se>
2021-01-13 14:35:49 +0100

    removing validators

--- backend/api/household/assets/validation/updatedb.js
+++ backend/api/household/assets/validation/updatedb.js
@@ -10,14 +10,14 @@ module.exports = function validateUpdateInput(data) {
  temp = !isEmpty(data.ratio) ? data.ratio : "";
 
 // ratio check
-  if (! (Validator.isEmpty(temp))) {
-    if (Validator.isFloat(temp)) {
-      errors.ratio = "Ratio needs to be a number";
-    }
+//  if (! (Validator.isEmpty(temp))) {
+//    if (Validator.isFloat(temp)) {
+//      errors.ratio = "Ratio needs to be a number";
+//    }
     if(!(0 <= data.ratio <= 1)){
       errors.ratio = "ratio need to be between 0 and 1.";
     }
-  }
+//  }
 
 return {
     errors,
