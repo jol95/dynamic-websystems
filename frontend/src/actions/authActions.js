9ba67df852808c6b628ba8b79432e0f9936f669a
Mans <mnsosk-7@student.ltu.se>
2021-01-13 12:10:43 +0100

    syntax fix

--- frontend/src/actions/authActions.js
+++ frontend/src/actions/authActions.js
@@ -9,7 +9,7 @@ import {
 
 // Update ratio
 export const updateDatabase = (dbData) => dispatch => {
-  axios.put("api/household/" + houseID, dbData)
+  //axios.put("api/household/" + houseID, dbData)
 };
 
 
@@ -40,8 +40,8 @@ export const loginUser = (userData, history) => dispatch => {
       const decoded = jwt_decode(token);
       // Set current user
       dispatch(setCurrentUser(decoded));
-      console.log(decoded);
-      console.log(setCurrentUser(decoded));
+      console.log("decoded" decoded);
+      console.log("payload" setCurrentUser(payload));
       history.push("/dashboard");
     })
     .catch(err =>
