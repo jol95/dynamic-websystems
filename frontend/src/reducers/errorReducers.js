90140ecea8e3ce07ab11f5a0f972257b79fef024
Mans <mnsosk-7@student.ltu.se>
2021-01-14 16:05:15 +0100

    wild testing

--- frontend/src/reducers/errorReducers.js
+++ frontend/src/reducers/errorReducers.js
@@ -1,4 +1,6 @@
-import { GET_ERRORS } from "../actions/types";
+import { GET_ERRORS,
+         CURRENT_TYPE 
+        } from "../actions/types";
 
 const initialState = {};
 
@@ -6,6 +8,12 @@ export function errorR (state = initialState, action) {
   switch (action.type) {
     case GET_ERRORS:
       return action.payload;
+    case CURRENT_TYPE:
+      return {
+        ...state,
+        isAuthenticated: !isEmpty(action.payload),
+        user: action.payload
+      };
     default:
       return state;
   }
