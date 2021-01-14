d16677616d8954e4190e0562b19973ea440f07a5
Mans <mnsosk-7@student.ltu.se>
2021-01-15 00:25:32 +0100

    pure yollo on the households

--- frontend/src/containers/households/Households.js
+++ frontend/src/containers/households/Households.js
@@ -1,26 +1,73 @@
-import React, { useState } from "react";
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { displayDatabase } from "../../actions/authActions";
 import axios from 'axios';
-import "./Households.css";
+import "./UserProfile.css";
 
-function Households(props){
-    const [userData, setUserData] = useState(null);
-  
-    const fetchData = async () => {
-      const response = await axios.get("/api/household");
-      setUserData(response.data);
+class HouseHolds extends Component {
+    constructor() {
+        super();
+        this.state = {
+            pollingCount: 0,
+            delay: 1000,
+            errors: {}
+        };
     }
-    
-  
-    return (
-    fetchData(),
-      <div className="Apphouse">
-          <h1>Household overviews</h1>
-          <h2>Show household info</h2>
-        {/* Fetch data from API */}
-        <br />
-        {/* Display data from API */}   
-        <div className="houses"> 
-            {userData && userData.map((houses, index) => {
+
+    componentDidMount() {
+        this.interval = setInterval(this.tick, this.state.delay);
+    }
+
+    componentDidUpdate(prevProps, prevState){
+        if (prevState.delay !== this.state.delay) {
+            clearInterval(this.interval);
+            this.interval = setInterval(this.tick, this.state.delay);
+        }
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.interval);
+    }
+
+
+    tick = async () => {    
+            //settimeout // polling
+            //const base64Flag = 'data:image/jpeg;base64,';
+            const { user } = this.props.auth;
+            const data = user.id
+            var kind = "household/"
+
+            }
+            const response = await axios.get("/api/" + kind + data);
+            this.setState({
+                pollingCount: this.state.pollingCount + 1,
+                id: response.data.id,
+                display:  response.data.img,
+                wind:  response.data.wind,
+                production:  response.data.production,
+                consumption:  response.data.consumption,
+                netproduction:  response.data.netproduction,
+                buffer:  response.data.buffer,
+                blackout:  response.data.blackout,
+                ratio:  response.data.ratio,
+            })
+    }
+
+
+
+    render() {
+        const { id, display, wind, production, consumption,
+        netproduction, buffer, blackout, ratio } = this.state
+        return(
+        <div className="Apphouse">
+            <h1>Your Household</h1>
+            <h2>Show household info</h2>
+            {/* Fetch data from API */}
+            <br />
+            {/* Display data from API */}   
+                <div className="houses"> 
+                {this.state && this.state.map((houses, index) => {
                 return (
                     <div className="house" key={index}>
                         <h3>House {index + 1}</h3>
@@ -42,8 +89,21 @@ function Households(props){
                 );
             })}    
         </div>
-        </div>
-    );
+        </div>   
+        );
+}
 }
 
-export default Households;
+UserProfile.propTypes = {
+    displayDatabase: PropTypes.func.isRequired,
+    auth: PropTypes.object.isRequired,
+    errors: PropTypes.object.isRequired
+  };
+  const mapStateToProps = state => ({
+    auth: state.auth,
+    errors: state.errors
+  });
+  export default connect(
+    mapStateToProps,
+    { displayDatabase }
+  )( UserProfile );
