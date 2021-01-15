0d163129e4131f20ea49ed15a1596e4685b9f8a4
Mans <mnsosk-7@student.ltu.se>
2021-01-15 17:29:41 +0100

    css is fun

--- frontend/src/containers/updatedb/UpdateDb.js
+++ frontend/src/containers/updatedb/UpdateDb.js
@@ -1,9 +1,8 @@
 import React, { Component } from "react";
-//import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { updateDatabase } from "../../actions/authActions";
-//import classnames from "classnames";
+import "./UpdateDb.css";
 
 class UpdateDb extends Component {
     constructor() {
