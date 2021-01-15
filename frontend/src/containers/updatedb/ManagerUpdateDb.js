e846e3b2db8021d1baebae34d00697ac515d525b
Mans <mnsosk-7@student.ltu.se>
2021-01-15 18:03:02 +0100

    status button

--- frontend/src/containers/updatedb/ManagerUpdateDb.js
+++ frontend/src/containers/updatedb/ManagerUpdateDb.js
@@ -90,6 +90,21 @@ class ManagerUpdateDb extends Component {
                 </select>      
 
               </div>   
+
+            {/* STATUS SUBMIT BUTTON */}
+            <div className="col s12" style={{ paddingLeft: "11.250px" }}>
+              <button
+                style={{
+                  width: "150px",
+                  borderRadius: "3px",
+                  letterSpacing: "1.5px",
+                  marginTop: "1rem"
+                }}
+                type="submit"
+                className="btn btn-large waves-effect waves-light hoverable blue accent-3"
+              >
+                Update Status
+                </button>
               </div>
             </form>
           </div>
