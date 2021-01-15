dad9d0a722bd92c24d302e1b9dcba6b4e1f99d2c
Mans <mnsosk-7@student.ltu.se>
2021-01-15 21:33:58 +0100

    trying to set state

--- frontend/src/containers/households/HouseholdUsers.js
+++ frontend/src/containers/households/HouseholdUsers.js
@@ -44,7 +44,7 @@ class HouseholdUsers extends Component {
     }
 
     onEditClick = e => {
-        console.log("click");
+        console.log("click", this.state.id);
         const value = this.state.id;
         const path = `/profile/${value}`;
         this.props.history.push(path);
@@ -85,7 +85,9 @@ class HouseholdUsers extends Component {
                             letterSpacing: "1.5px",
                             marginTop: "1rem"
                         }}
-                        onClick={(e) => this.state.id = item.state}
+                        onClick={(e) =>  this.setState({
+                            id: item.id})}
+
                         onClick={this.onEditClick}
 
                         className="btn btn-large waves-effect waves-light hoverable blue accent-3"
