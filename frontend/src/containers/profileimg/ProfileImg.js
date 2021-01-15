e29b1d97b406947772b80a77d21142344f555fdc
Mans <mnsosk-7@student.ltu.se>
2021-01-15 18:57:40 +0100

    img stuff

--- frontend/src/containers/profileimg/ProfileImg.js
+++ frontend/src/containers/profileimg/ProfileImg.js
@@ -88,15 +88,13 @@ class ProfileImg extends Component {
     {/*<UserProfile/>*/}
       <label class="custom-file">
         <input
-        style={{size: 60
-        }} 
           type="file"
           name="image"
           id="file"
           accept=".jpeg, .png, .jpg"
           onChange = {this.onChange}
         />
-        <label/>
+        </label>
         
         </div>
           <div className="col s12" style={{ paddingLeft: "11.250px" }}>
